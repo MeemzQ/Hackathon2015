14d5608441d318c589de9343bc08681abace8684
WentingPan <wenting.pan@mail.utoronto.ca>
2015-01-31 17:13:31 -0500

    Fixed void in FriendList

--- FriendZone/app/src/main/java/com/example/friendzone/FriendList.java
+++ FriendZone/app/src/main/java/com/example/friendzone/FriendList.java
@@ -4,9 +4,9 @@ package com.example.friendzone;
  * Created by Wenting Pan on 2015-01-31.
  */
 public class FriendList {
-    private Friend[] friend_list;
+    private Friend[] friend_list  = new Friend[];
 
-    public addFriend(Friend friend) {
+    public void addFriend(Friend friend) {
         this.friend_list.append(friend);
     }
 }
