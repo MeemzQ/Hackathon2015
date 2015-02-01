16974341f349d601ba09673eaa8f2d97601531b3
MeemzQ <meeralqureshi@hotmail.com>
2015-01-31 21:47:16 -0500

    Worked on some java classes

--- FriendZone/app/src/main/java/com/example/friendzone/Friend.java
+++ FriendZone/app/src/main/java/com/example/friendzone/Friend.java
@@ -6,14 +6,57 @@ package com.example.friendzone;
 public class Friend {
     private String first_name;
     private String last_name;
+    private String name;
     private int phone_number;
 
     public Friend(String first, String last, int number) {
         this.first_name = first;
         this.last_name = last;
+        this.name = first + " " +  last;
         this.phone_number = number;
 
     }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+/*    public int compareTo(Object obj) {
+        if (((Friend) obj).getName() == this.getName())
+            return 0;
+        else if (((Friend) obj).getName() > this.getName())
+            return -1;
+        else
+            return 1;
+    }*/
 
     //Need status (in database or not) and get gps location
 }
--- FriendZone/app/src/main/java/com/example/friendzone/FriendList.java
+++ FriendZone/app/src/main/java/com/example/friendzone/FriendList.java
@@ -1,12 +1,69 @@
 package com.example.friendzone;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static java.util.Collections.*;
+
 /**
  * Created by Wenting Pan on 2015-01-31.
  */
 public class FriendList {
-    private Friend[] friend_list  = new Friend[];
 
-    public void addFriend(Friend friend) {
-        this.friend_list.append(friend);
+    private ArrayList<Friend> friend_list;
+    private ArrayList<Friend> favourite_list;
+    private ArrayList<Friend> sort_friends;
+
+    public FriendList(){
+        this.friend_list = new ArrayList<Friend>();
     }
+
+    public void addFriend(Friend friend) { // When we add we need to create
+        this.friend_list.add(friend);
+    }
+
+
+    public void removeFriend(Friend friend){
+        this.friend_list.remove(friend);
+    }
+
+    public ArrayList<Friend> getFriend_list() {
+        return friend_list;
+    }
+
+    public void setFriend_list(ArrayList<Friend> friend_list) {
+        this.friend_list = friend_list;
+    }
+
+    public ArrayList<Friend> getFavourite_list() {
+        return favourite_list;
+    }
+
+    public void setFavourite_list(ArrayList<Friend> favourite_list) {
+        this.favourite_list = favourite_list;
+    }
+
+    public ArrayList<Friend> getSort_friends() {
+        return sort_friends;
+    }
+
+    public void setSort_friends(ArrayList<Friend> friends_list) {
+        this.sort_friends = friends_list;
+    }
+
+/*    public ArrayList<Friend> sortList(ArrayList<Friend> part_list){
+        return sort(part_list);
+    }*/
+
+    public ArrayList<Friend> arranged() {
+        for (Friend f : this.favourite_list) {
+            this.sort_friends.add(f);
+        }
+        for (Friend f : this.friend_list) {
+            this.sort_friends.add(f);
+        }
+
+        return this.sort_friends;
+    }
+
 }
